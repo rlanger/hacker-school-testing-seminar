93aaf8d241a07827d2cba19aac17a55e401d78e8
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-07-02 17:38:45 -0400

    Way better tests for parsing time, drawing right drawing, drawing to canvas.
    * Can now test all the different cases of the different pieces of the app.
    * Added async testing to tester.js
    * Added beforeEach() to tester.js
    * Added mock() to tester.js
    * Used these features to write better tests.

--- tests/client_tests.js
+++ tests/client_tests.js
@@ -1,40 +1,83 @@
 ;(function() {
   var tester = require('./tester').tester;
   var client = require('../client');
+  var canvasRenderer = require('../canvas-renderer').canvasRenderer;
 
-  tester.test(
-    'should draw sun and blue sky in canvas when it is daytime', function() {
-      global.XMLHttpRequest = function() {
-        this.open = function() {};
-
-        this.send = function() {
-          this.onload({
-            target: { responseText: '{ "time": "day" }' }
-          });
-        };
+  // parsing time
+  tester.test({
+    'should parse time from server': function(done) {
+      client.get = function(_, callback) {
+        callback({ target: { responseText: '{ "time": "day" }' }});
       };
 
+      client.getTime(function(time) {
+        tester.isEqual(time, "day");
+        done();
+      });
+    }
+  });
+
+  // canvas
+  tester.test({
+    'should draw rect where asked when fillBackground() called': function() {
+      var ctx = {
+        fillRect: tester.mock(0, 0, 300, 150),
+        canvas: { width: 300, height: 150 }
+      };
+      canvasRenderer.fillBackground(ctx, "#aaa");
+      tester.isEqual(ctx.fillStyle, "#aaa");
+    },
+
+    'should draw circile where asked when fillCircle() called': function() {
+      var ctx = {
+        beginPath: function() {},
+        closePath: function() {},
+        fill: function() {},
+        arc: tester.mock(150, 75, 30, 0, Math.PI * 2),
+      };
+      canvasRenderer.fillCircle(ctx, 150, 75, 30, "#bbb");
+      tester.isEqual(ctx.fillStyle, "#bbb");
+    }
+  });
+
+  // correct drawing for time
+  tester.test({
+    beforeEach: function() {
       global.document = {
         getElementById: function() { return { getContext: function() {}}}
       };
 
       global.canvasRenderer = {
-        center: function() { return { x: 150, y: 75 } },
-
-        fillBackground: function(_, color) {
-          tester.isEqual(color, "#00f");
-        },
-
-        fillCircle: function(_, x, y, radius, color) {
-          tester.isEqual(x, 150);
-          tester.isEqual(y, 75);
-          tester.isEqual(radius, 30);
-          tester.isEqual(color, "#ff0");
+        center: function() {
+          return { x: 150, y: 75 };
         }
       };
+    },
 
-      client.loadTime();
+    'should draw circle in the centre of sky when day': function() {
+      global.canvasRenderer.fillBackground = function() { };
+      global.canvasRenderer.fillCircle = tester.mock(tester.SKIP, 150, 75, 30, tester.SKIP);
+      client.displayTime("day");
+    },
+
+    'should draw circle in the centre of sky when night': function() {
+      global.canvasRenderer.fillBackground = function() { };
+      global.canvasRenderer.fillCircle = tester.mock(tester.SKIP, 150, 75, 30, tester.SKIP);
+      client.displayTime("night");
+    },
+
+    'should draw sun and blue sky when it is daytime': function() {
+      global.canvasRenderer.fillBackground = tester.mock(tester.SKIP, "#00f");
+      global.canvasRenderer.fillCircle = tester.mock(tester.SKIP, tester.SKIP,
+                                                     tester.SKIP, tester.SKIP, "#ff0");
+      client.displayTime("day");
+    },
+
+    'should draw sun and blue sky when it is nighttime': function() {
+      global.canvasRenderer.fillBackground = tester.mock(tester.SKIP, "#000");
+      global.canvasRenderer.fillCircle = tester.mock(tester.SKIP, tester.SKIP,
+                                                     tester.SKIP, tester.SKIP, "#aaa");
+      client.displayTime("night");
     }
-  );
-
+  });
 })(this);
--- tests/tester.js
+++ tests/tester.js
@@ -3,30 +3,58 @@
     return !isNaN(parseFloat(n)) && isFinite(n);
   };
 
+  var isString = function(str) {
+    return typeof str === 'string';
+  };
+
   var isNumbersEqual = function(a, b) {
     return a === b;
   };
 
-  var gatherTests = function(testArray) {
-    return Array.prototype.slice.call(testArray).reduce(function(acc, x, i) {
-      i % 2 === 0 ? acc.push({ description:x }) : acc[acc.length-1].fn = x;
-      return acc;
-    }, []);
+  var passed = function() {
+    process.stdout.write(".");
+  };
+
+  var failed = function(test, exception) {
+    console.log(test.description);
+    console.log(exception.stack);
+  };
+
+  var testSync = function(test) {
+    test();
+    passed();
+  };
+
+  var testAsync = function(test) {
+    var timeout = setTimeout(function() {
+      try {
+        throw new Error("Failed: done() never called in async test.");
+      } catch (e) {
+        failed(test, e);
+      }
+    }, 500);
+
+    test(function() {
+      clearTimeout(timeout);
+      passed();
+    });
   };
 
   exports.tester = {
     test: function(tests) {
-      var tests = gatherTests(arguments);
-      for (var i = 0; i < tests.length; i++) {
+      var beforeEach = tests.beforeEach;
+      delete tests.beforeEach;
+      for (var i in tests) {
+        if (beforeEach !== undefined) {
+          beforeEach();
+        }
         try {
-          tests[i].fn();
-          process.stdout.write(".");
+          tests[i].length === 1 ? testAsync(tests[i]) : testSync(tests[i]);
         } catch (e) {
-          console.log(tests[i].description);
-          console.log(e.stack);
+          failed(tests[i], e);
         }
       }
-      console.log();
+      console.log()
     },
 
     isEqual: function(a, b) {
@@ -34,7 +62,30 @@
         if (isNumbersEqual(a, b) === false) {
           throw new Error(a + " and " + b + " are not equal");
         }
+      } else if (isString(a) && isString(b)) {
+        if (a !== b) {
+          throw new Error(a + " and " + b + " are not equal");
+        }
       }
-    }
+    },
+
+    mock: function() {
+      var expectedArgs = Array.prototype.slice.call(arguments);
+      var self = this;
+      return function() {
+        var actualArgs = Array.prototype.slice.call(arguments);
+        if (expectedArgs.length !== actualArgs.length) {
+          throw new Error("Wrong number of args to mocked function");
+        }
+
+        for (var i = 0; i < expectedArgs.length; i++) {
+          if (expectedArgs[i] !== self.SKIP) {
+            self.isEqual(expectedArgs[i], actualArgs[i]);
+          }
+        }
+      };
+    },
+
+    SKIP: 'o89eu9o8ehu9o8ehu98hoeu98hoeu98hoeu98hoe98uho.98hu9,.'
   };
 })(this);
