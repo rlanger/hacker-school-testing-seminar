b27d959cc5f2db38c896d9c5f50948327244f926
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-07-01 21:29:40 -0400

    Make big test less big with a canvasRenderer mock.
    * No need to mock canvas, any more.
    * Mock canvasRenderer fns and check they get called with right args.

--- tests/client_tests.js
+++ tests/client_tests.js
@@ -15,36 +15,21 @@
       };
 
       global.document = {
-        getElementById: function() {
-          return {
-            getContext: function() {
-              return {
-                beginPath: function() {},
-                closePath: function() {},
-                fill: function() {},
-                canvas: {
-                  width: 300,
-                  height: 150
-                },
+        getElementById: function() { return { getContext: function() {}}}
+      };
 
-                fillRect: function(x, y, w, h) {
-                  tester.isEqual(x, 0);
-                  tester.isEqual(y, 0);
-                  tester.isEqual(w, 300);
-                  tester.isEqual(h, 150);
-                  tester.isEqual(this.fillStyle, "#ff0");
-                },
+      global.canvasRenderer = {
+        center: function() { return { x: 150, y: 75 } },
 
-                arc: function(x, y, radius, start, stop) {
-                  tester.isEqual(x, 150);
-                  tester.isEqual(y, 75);
-                  tester.isEqual(radius, 30);
-                  tester.isEqual(start, 0);
-                  tester.isEqual(stop, Math.PI * 2);
-                }
-              };
-            }
-          };
+        fillBackground: function(_, color) {
+          tester.isEqual(color, "#00f");
+        },
+
+        fillCircle: function(_, x, y, radius, color) {
+          tester.isEqual(x, 150);
+          tester.isEqual(y, 75);
+          tester.isEqual(radius, 30);
+          tester.isEqual(color, "#ff0");
         }
       };
 
